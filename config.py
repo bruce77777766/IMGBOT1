9f0832c1e4b437da7d2dc7bce492e41ee56b7fc7
bruce77777766 <143010659+bruce77777766@users.noreply.github.com>
2024-12-01 01:55:24 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Change all the values above to your own.
+
+class Cfg:
+    api_id = '23474052' # Your api_id from my.telegram.org
+    api_hash = '32c7a1dc82cefed3c9e4b907cc868e18' # Your api_hash from my.telegram.org
+    bot_token = '7750654765:AAF2Yzey7O_iO9R8gkVm_1GOavUuSqnSVtw' # Your bot token from @botfather
+    client_id = 'a78dbcdbc7f8888' # Your client_id from api.imgur.com
+    client_secret = '4ca114572ad47315310d9b3ffbaedf80a00c6291' # Your client_secret from api.imgur.com
